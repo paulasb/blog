5580f7f07293971fac959bc0c17b191c7cb57688
Paula <paulasb18@gmail.com>
2018-02-20 15:20:11 -0300

    algumas páginas criadas, links entre as páginas, criação e visualização de post ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,34 @@
 class ArticlesController < ApplicationController
+	def index #para ver todos os articles
+    	@articles = Article.all
+  	end
+  	
+	def show #edireciona-se o ususario para o show action acima
+    	@article = Article.find(params[:id])
+  	end
+
 	def new
+ 	end
+	
+	def create # para usar o Article model para salvar os dados no banco de dados
+		#params[:article] possui os atributos do form 
+		#antes estava assim: @article = Article.new(params[:article])
+		#então cria-se a funcao "article_params" abaixo e fica assim: @article = Article.new(article_params) 
+	  	#isso é feito para quais parametros sao permitidos na ação do controller
+	  	@article = Article.new(article_params) 
+	 
+	  	@article.save
+	  	redirect_to @article
 	end
-end
+	 
+	private
+	  def article_params
+	    params.require(:article).permit(:title, :text)#para eivtar que violem os campos do form?
+	  end
+
+	#mostra os parametros (atributos) do formulario, os valores inseridos
+	#def create
+	  #render plain: params[:article].inspect
+	#end
+	end
+
