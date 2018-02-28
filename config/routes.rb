dc2bf4169ef8a1fa9b98780a2cc565ce002b0cb2
Paula <paulasb18@gmail.com>
2018-02-28 18:33:40 -0300

    autenticacao ok, bootstrap ok, menu superior ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  #devise_for :users
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  get 'welcome/index'
+  devise_for :users
+  #devise_for :users, controllers: {
+  #  sessions: 'users/sessions'
+  #}
+  # get 'welcome/index'
   resources :articles do
   	 resources :comments
   end
   root 'welcome#index'
+  # para ir para os posts quando clica na barra superior
+  # root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
