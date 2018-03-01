917dc66995bd2cdfa3c6fcb771a7151e521e444f
Paula <paulasb18@gmail.com>
2018-03-01 18:57:01 -0300

    alterações realizadas

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 20180228135219) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
